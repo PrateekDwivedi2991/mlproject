434ac627df0c5edf21684b59a41d413345de0625
PrateekDwivedi2991 <dwivedi.prateek@hotmail.com>
2023-08-13 02:37:58 +0530

    last update

--- setup.py
+++ setup.py
@@ -4,21 +4,24 @@ from typing import List
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function wil return the list of requirements
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
-        
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Prateek',
-    author_email='dwivediprateekms@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
